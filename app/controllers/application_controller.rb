d5d398cdb4dd5f55957a753b0034cc3da75b6f51
Shakhboz <shfazliddinov1940@gmail.com>
2024-11-23 17:04:36 +0500

    m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   before_action :authenticate_user!
+  before_action :check_access
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
+
+  def check_access
+    return unless ENV.fetch('STOP_PROCESSING', nil).present?
+
+    render plain: 'Application error', status: :forbidden
+  end
 
   def user_not_authorized
     flash[:alert] = 'Вам не разрешено совершить эту операцию.'
