8e0c55271c334f8d20ae1de8b4da423e0b5ba702
Shakhboz <Shfazliddinov1940@gmail.com>
2025-02-24 09:52:25 +0500

    m

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+# ok
 Bundler.require(*Rails.groups)
 
 module PaperApp
